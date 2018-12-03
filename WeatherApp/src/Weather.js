cb97e4d262a383c5e13711b0d43f026dc6b3495b
Fan Huang <fan.huang.33@gmail.com>
2018-12-02 22:25:23 -0500

    added summary

--- WeatherApp/src/Weather.js
+++ WeatherApp/src/Weather.js
@@ -7,12 +7,17 @@ class WeatherApp extends React.Component {
         super(props);
         this.state = {
             data: null,
-            temp: 49.08
+            temp: null,
+            summary: null
         };
     }
+    
 
-    getWeather(e) {
-        e.preventDefault();
+    getWeather(e=null) {
+        if (e) {
+            e.preventDefault();
+        }
+        
         var that = this;
         fetch('/weather')
             .then(data => data.json())
@@ -24,14 +29,32 @@ class WeatherApp extends React.Component {
             });
         
     }
+    componentDidMount() {
+        var that = this;
+        fetch('/weather')
+            .then(data => data.json())
+            .then(function(res) {
+                that.setState({
+                    data: res,
+                    temp: res.currently.apparentTemperature,
+                    summary: res.currently.summary
+                });
+            });
+    }
+
 
     render() {
-        let tempDisplay = `${this.state.temp} Fahrenheit`;
+        let tempDisplay;
+        if (this.state.data != null) {
+            tempDisplay = `${this.state.temp} Fahrenheit`;
+        }
+        
         return (
             <div>
-
-                <Temperature temp={tempDisplay} ></Temperature>
+                <h1>Weather App</h1>
                 <Form loadWeather={this.getWeather.bind(this)}></Form>
+                <Temperature temp={tempDisplay} summary={this.state.summary}></Temperature>
+                
                 
             </div>
             
@@ -44,7 +67,8 @@ class Temperature extends React.Component {
     render () {
         return (
             <div className="WeatherApp">
-                <h1>{ this.props.temp }</h1>
+                <h2>{ this.props.summary }</h2>
+                <h2>{ this.props.temp }</h2>
             </div>
         );
     }
@@ -55,7 +79,8 @@ class Form extends React.Component {
     render() {
         return (
             <form onSubmit = {this.props.loadWeather}>
-                <input type="text" name="location" placeholder="Zip Code..."/>
+                <input type="text" name="longitude" placeholder="Longitude"/>
+                <input type="text" name="latitude" placeholder="Latitude"/>
                 <button>Get Weather</button>
             </form>
         );
--- server.js
+++ server.js
@@ -6,21 +6,22 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/weather', (req, res) => {
-  const name = req.query.name || 'World';
-  var latitude = 42.3601;
-  var longitude = -71.0589;
-  var API_KEY = process.env.DARKSKYAPI
-  request({
-      method: 'GET',
-      uri: `https://api.darksky.net/forecast/${API_KEY}/${latitude},${longitude}`,
-      'content-type': 'application/json'
-  },
-  function(error, response, body) {
-    if (error) {
-        console.log(error)
-    }
-    return res.send(body);
-  });
+    var API_KEY = process.env.DARKSKYAPI
+    var latitude = 42.3601;
+    var longitude = -71.0589;
+  
+    request({
+        method: 'GET',
+        uri: `https://api.darksky.net/forecast/${API_KEY}/${latitude},${longitude}`,
+        'content-type': 'application/json'
+    },
+    function(error, response, body) {
+        if (error) {
+            console.log(error);
+            res.status(500).send(error);
+        }
+        return res.status(200).send(body);
+    });
 
   
 });
