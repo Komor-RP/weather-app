1ef64f634ff9de18abf1298a6c7c245531d2af9c
Fan Huang <fan.huang.33@gmail.com>
2018-12-08 09:23:04 -0500

    Added font awesome icons to the daily display

--- WeatherApp/src/Weather.js
+++ WeatherApp/src/Weather.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Weather.css';
 import * as moment from  'moment';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAbacus, faCloudDrizzle, faCloudRain, faClouds, faCloudSnow, faFog, faCloudMoon, faCloudSun, faCloud } from '@fortawesome/pro-regular-svg-icons';
 
 
 class WeatherApp extends React.Component {
@@ -81,36 +83,43 @@ class WeekDisplay extends React.Component {
             <div className="WeekDisplay">
                 <DayDisplay
                     day={ now }
+                    icon={ this.props.weather ? this.props.weather.data[0].icon : null}
                     temp={ this.props.weather ? 
                         `${this.props.weather.data[0].temperatureLow}-${this.props.weather.data[0].temperatureHigh}` : null }>
                 </DayDisplay>
                 <DayDisplay
                     day={ moment(new Date()).add(1, 'days').format('dddd') }
+                    icon={ this.props.weather ? this.props.weather.data[1].icon : null}
                     temp={ this.props.weather ? 
                         `${this.props.weather.data[1].temperatureLow}-${this.props.weather.data[1].temperatureHigh}` : null }>
                 </DayDisplay>
                 <DayDisplay
                     day={ moment(new Date()).add(2, 'days').format('dddd') }
+                    icon={ this.props.weather ? this.props.weather.data[2].icon : null}
                     temp={ this.props.weather ? 
                         `${this.props.weather.data[2].temperatureLow}-${this.props.weather.data[2].temperatureHigh}` : null }>
                 </DayDisplay>
                 <DayDisplay
                     day={ moment(new Date()).add(3, 'days').format('dddd') }
+                    icon={ this.props.weather ? this.props.weather.data[3].icon : null}
                     temp={ this.props.weather ? 
                         `${this.props.weather.data[3].temperatureLow}-${this.props.weather.data[3].temperatureHigh}` : null }>
                 </DayDisplay>
                 <DayDisplay
                     day={ moment(new Date()).add(4, 'days').format('dddd') }
+                    icon={ this.props.weather ? this.props.weather.data[4].icon : null}
                     temp={ this.props.weather ? 
                         `${this.props.weather.data[4].temperatureLow}-${this.props.weather.data[4].temperatureHigh}` : null }>
                 </DayDisplay>
                 <DayDisplay
                     day={ moment(new Date()).add(5, 'days').format('dddd') }
+                    icon={ this.props.weather ? this.props.weather.data[5].icon : null}
                     temp={ this.props.weather ? 
                         `${this.props.weather.data[5].temperatureLow}-${this.props.weather.data[5].temperatureHigh}` : null }>
                 </DayDisplay>
                 <DayDisplay
                     day={ moment(new Date()).add(6, 'days').format('dddd') }
+                    icon={ this.props.weather ? this.props.weather.data[6].icon : null}
                     temp={ this.props.weather ? 
                         `${this.props.weather.data[6].temperatureLow}-${this.props.weather.data[6].temperatureHigh}` : null }>
                 </DayDisplay>
@@ -121,12 +130,34 @@ class WeekDisplay extends React.Component {
 
 
 class DayDisplay extends React.Component {
+
+    choooseIcon() {
+        switch( this.props.icon ) {
+            case 'rain':
+                return faCloudRain;
+            case 'cloudy':
+                return faClouds;
+            case 'snow':
+                return faCloudSnow;
+            case 'fog':
+                return faFog;
+            case 'partly-cloudy-night':
+                return faCloudMoon;
+            case 'partly-cloudy-day':
+                return faCloudSun;
+            default:
+                return faCloud;
+        }
+        
+    }
+
+
     render () {
         
         return (
-            <div>
+            <div className="DayDisplay">
                 <h4>{ this.props.day }</h4>
-
+                <FontAwesomeIcon icon={this.choooseIcon()} />
                 <h3>{ this.props.temp}</h3>
             </div>
 
@@ -136,10 +167,13 @@ class DayDisplay extends React.Component {
 
 class Temperature extends React.Component {
 
+    
+
     render () {
         return (
             <div className="WeatherApp">
                 <h2>{ this.props.summary }</h2>
+                
                 <h2>{ this.props.temp }</h2>
             </div>
         );
