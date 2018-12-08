4b64409921f3908e6eb613301221fdd964c9a5ed
Fan Huang <fan.huang.33@gmail.com>
2018-12-07 23:46:38 -0500

    added dotenv for api keys

--- WeatherApp/src/Weather.js
+++ WeatherApp/src/Weather.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Weather.css';
+import * as moment from  'moment';
+
 
 class WeatherApp extends React.Component {
     constructor(props) {
@@ -8,14 +10,14 @@ class WeatherApp extends React.Component {
         this.state = {
             data: null,
             temp: null,
-            summary: null
+            summary: null,   
         };
     }
     
 
-    getWeather(latitude, longitude) {
+    getWeather(location) {
         var that = this;
-        const url = `/weather?latitude=${latitude}&longitude=${longitude}`;
+        const url = `/weather?location=${location}`;
 
         fetch(url)
             .then(data => data.json())
@@ -23,12 +25,12 @@ class WeatherApp extends React.Component {
                 that.setState({
                     data: res,
                     temp: res.currently.apparentTemperature,
-                    summary: res.currently.summary
+                    summary: res.currently.summary,
                 });
             });
         
     }
-    componentDidMount() {
+    componentWillMount() {
         var latitude = 20.3601;
         var longitude = -71.0589;
         this.getWeather(latitude, longitude);
@@ -37,28 +39,98 @@ class WeatherApp extends React.Component {
     locationSearch(e) {
         e.preventDefault();
         const data = new FormData(e.target);
-        var latitude = data.get('latitude');
-        var longitude = data.get('longitude');
-        this.getWeather(latitude, longitude);
+        let location = data.get('location');
+        this.getWeather(location);
     }
 
 
     render() {
         let tempDisplay;
+        let weekTemp;
         if (this.state.data != null) {
             tempDisplay = `${this.state.temp} Fahrenheit`;
+            weekTemp = this.state.data.daily;
         }
         
         return (
             <div>
                 <h1>Weather App</h1>
                 <Form loadWeather={this.locationSearch.bind(this)}></Form>
+                <TimeDisplay></TimeDisplay>
                 <Temperature temp={tempDisplay} summary={this.state.summary}></Temperature>
-                
+                <WeekDisplay weather={ weekTemp }></WeekDisplay>
                 
             </div>
             
         );
+    }
+}
+
+class WeekDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+    //pass the props down to day display
+    render () {
+        let now = moment().format('dddd');
+
+        return (
+            <div className="WeekDisplay">
+                <DayDisplay
+                    day={ now }
+                    temp={ this.props.weather ? 
+                        `${this.props.weather.data[0].temperatureLow}-${this.props.weather.data[0].temperatureHigh}` : null }>
+                </DayDisplay>
+                <DayDisplay
+                    day={ moment(new Date()).add(1, 'days').format('dddd') }
+                    temp={ this.props.weather ? 
+                        `${this.props.weather.data[1].temperatureLow}-${this.props.weather.data[1].temperatureHigh}` : null }>
+                </DayDisplay>
+                <DayDisplay
+                    day={ moment(new Date()).add(2, 'days').format('dddd') }
+                    temp={ this.props.weather ? 
+                        `${this.props.weather.data[2].temperatureLow}-${this.props.weather.data[2].temperatureHigh}` : null }>
+                </DayDisplay>
+                <DayDisplay
+                    day={ moment(new Date()).add(3, 'days').format('dddd') }
+                    temp={ this.props.weather ? 
+                        `${this.props.weather.data[3].temperatureLow}-${this.props.weather.data[3].temperatureHigh}` : null }>
+                </DayDisplay>
+                <DayDisplay
+                    day={ moment(new Date()).add(4, 'days').format('dddd') }
+                    temp={ this.props.weather ? 
+                        `${this.props.weather.data[4].temperatureLow}-${this.props.weather.data[4].temperatureHigh}` : null }>
+                </DayDisplay>
+                <DayDisplay
+                    day={ moment(new Date()).add(5, 'days').format('dddd') }
+                    temp={ this.props.weather ? 
+                        `${this.props.weather.data[5].temperatureLow}-${this.props.weather.data[5].temperatureHigh}` : null }>
+                </DayDisplay>
+                <DayDisplay
+                    day={ moment(new Date()).add(6, 'days').format('dddd') }
+                    temp={ this.props.weather ? 
+                        `${this.props.weather.data[6].temperatureLow}-${this.props.weather.data[6].temperatureHigh}` : null }>
+                </DayDisplay>
+            </div>
+        );
+    }
+}
+
+
+class DayDisplay extends React.Component {
+    render () {
+        
+        return (
+            <div>
+                <h4>{ this.props.day }</h4>
+
+                <h3>{ this.props.temp}</h3>
+            </div>
+
+        )
     }
 }
 
@@ -74,13 +146,48 @@ class Temperature extends React.Component {
     }
 }
 
+class TimeDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+
+        }
+    }
+
+    // componentDidMount() {
+    //     this.timerID = setInterval(
+    //         () => this.tick(),
+    //         1000
+    //     );
+    // }
+
+    // componentWillUnmount() {
+    //     clearInterval(this.timerID)
+    // }
+
+    // tick() {
+    //     this.setState({
+    //         date: new Date()
+    //     });
+    // }
+
+    render () {
+        
+        return (
+            <div className="TimeDisplay">
+                <h2>{this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
+}
+
 class Form extends React.Component {
 
     render() {
         return (
             <form onSubmit = {this.props.loadWeather}>
-                <input type="text" name="longitude" placeholder="Longitude"/>
-                <input type="text" name="latitude" placeholder="Latitude"/>
+                <input type="text" name="location" placeholder="Location"/>
                 <button>Get Weather</button>
             </form>
         );
--- server.js
+++ server.js
@@ -1,26 +1,69 @@
 const express = require('express');
 var request = require('request');
+require('dotenv').config({ path: '.env.development'});
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+    res.header('Acess-Control-Allow-Origin', '*');
+    res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
-app.get('/weather', (req, res) => {
-    var API_KEY = process.env.DARKSKYAPI
-    var latitude = req.query.latitude;
-    var longitude = req.query.longitude;
+
+
+app.get('/location', (req, res) => {
+
+});
+
+
+function getLocation(callback, location, res) {
+    let MAPQUEST_KEY = process.env.MAPQUEST_KEY;
+    request({
+        method: 'GET',
+        uri: `http://www.mapquestapi.com/geocoding/v1/address?key=${MAPQUEST_KEY}&location=${location}`,
+        'content-type': 'application/json'
+    },
+    function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            data = JSON.parse(body);
+            let latitude = data.results[0].locations[0].latLng.lat;
+            let longitude = data.results[0].locations[0].latLng.lng;
+            console.log(longitude);
+            callback(latitude, longitude, res);
+        } else {
+            console.log("There was an error getting the coordinates.");
+            console.log(error);
+        }
+
+    });
+}
+
+function getWeather(latitude, longitude, res) {
+    let API_KEY = process.env.DARKSKY_KEY;
+
     request({
         method: 'GET',
         uri: `https://api.darksky.net/forecast/${API_KEY}/${latitude},${longitude}`,
         'content-type': 'application/json'
     },
     function(error, response, body) {
-        if (error) {
+        if (!error && response.statusCode === 200) {
+            return res.status(200).send(body);
+        } else {
+            console.log("There was an error getting the weather data.");
             console.log(error);
-            res.status(500).send(error);
+            return res.status(500).send(error);
         }
-        return res.status(200).send(body);
     });
+}
+
+app.get('/weather', (req, res) => {
+    
+    let location = req.query.location;
+    console.log("location", location);
+    return getLocation(getWeather, location, res);
 
   
 });
